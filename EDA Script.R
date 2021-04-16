80fb9f4d00cc93fd3a85019e99252742f0886db2
Phil Hagen <phillip.hagen@go.mnstate.edu>
2021-04-16 14:42:36 -0500

    Commit

--- EDA Script.R
+++ EDA Script.R
@@ -1,8 +1,11 @@
 # Load Packages ----------------------------------------------------------------
 
 library(tidyverse)
-
 library(readxl)
+
+# install.packages("corrplot")
+library(corrplot)
+library(ggplot2)
 
 # Data -------------------------------------------------------------------------
 download_data <- read_excel("data/2020 County Health Rankings North Dakota Data - v1_1 (2).xlsx", 
@@ -12,12 +15,48 @@ download_data
 
 glimpse(download_data)
 
-transmute(
+Ranked_meas_data <- transmute(
   download_data,
   FIPS,
   State,
   County,
   Deaths,
-  FP_health = `% Fair or Poor Health`
-
+  FP_health = `% Fair or Poor Health`,
+  Smokers = `% Smokers`,
+  Obesity_rate = `% Adults with Obesity`,
+  Excess_Drinking = `% Excessive Drinking`,
+  Physically_Inactive = `% Physically Inactive`,
+  Unemployment = `% Unemployed`,
+  Housing_Problems = `% Severe Housing Problems`,
+  Income = `Income Ratio`,
+  Violent_Crime = `Violent Crime Rate`
 )
+
+Ranked_meas_data
+
+# Plot--------------------------------------------------------------------------
+
+Ranked_meas_data %>%
+  filter(!is.na(County)) %>%
+  select(-FIPS,-State,-County,-Deaths) %>%
+  cor(use = "na.or.complete") %>%
+  corrplot(
+    method = "color",
+    type = "upper",
+    order = "hclust",
+    tl.col = "black",
+    tl.srt = 45,
+    addCoef.col = "black"
+  ) 
+  
+ggplot(Ranked_meas_data, aes(x=Violent_Crime, y=Excess_Drinking)) + 
+  geom_point() +
+  geom_smooth(method = lm, se = TRUE)
+
+ggplot(Ranked_meas_data, aes(x = Housing_Problems, y = Smokers)) + 
+  geom_point() + 
+  geom_smooth(method = lm, se = TRUE)
+
+ggplot(Ranked_meas_data, aes(x = Violent_Crime, y = FP_health)) +
+  geom_point() +
+  geom_smooth(method = lm, se = TRUE)
